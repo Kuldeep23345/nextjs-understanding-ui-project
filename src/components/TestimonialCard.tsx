0ebcfa1c193602cc81d5c6a730a602af24c5647d
Kuldeep <kunalrajput67718@gmail.com>
2025-09-14 20:42:21 +0530

    base done

---
+++ src/components/TestimonialCard.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+
+const musicSchoolTestimonials = [
+  {
+    quote:
+      "Joining the music school transformed my understanding of music and helped me to truly discover my own sound. The instructors are world-class!",
+    name: "Alex Johnson",
+    title: "Guitar Student",
+  },
+  {
+    quote:
+      "The community and support at this school are unmatched. I've grown not just as a pianist, but also as a performer, thanks to their comprehensive approach.",
+    name: "Samantha Lee",
+    title: "Piano Student",
+  },
+  {
+    quote:
+      "This school offered me the tools and confidence to take my singing to the next level. I'm endlessly grateful for the personalized coaching.",
+    name: "Michael Chen",
+    title: "Vocal Student",
+  },
+  {
+    quote:
+      "As a violinist, finding the right mentor can be challenging, but this school matched me with a teacher who truly understands my goals and challenges.",
+    name: "Emily Taylor",
+    title: "Violin Student",
+  },
+  {
+    quote:
+      "The production courses here opened my eyes to the intricacies of music production. Highly recommend for any aspiring producers!",
+    name: "Chris Morales",
+    title: "Music Production Student",
+  },
+];
+
+const TestimonialCard = () => {
+  return (
+
+
+   
+    <div
+      className={cn(
+        "[background-size:40px_40px]",
+        "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+        "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        "flex items-center justify-between py-5 md:py-40 w-full md:h-[40rem] h-[20rem] flex-col overflow-hidden" // Add your additional class here
+      )}
+    >
+    
+      <h1 className=" mb-3 text-lg md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400">
+         Hear our Harmony:Voices of Success
+        </h1>
+      <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-6xl">
+          <InfiniteMovingCards
+            items={musicSchoolTestimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+      </div>
+    </div>
+  
+  );
+};
+
+export default TestimonialCard;
